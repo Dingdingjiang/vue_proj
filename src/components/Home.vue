f0f5400a927770e645728f762b3dc8fe81eb21be
Dingdingjiang <45352527+Dingdingjiang@users.noreply.github.com>
2021-06-28 00:09:43 +0800

    complete Login Form Box

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>这是home</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="login_contain">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" />
+      </div>
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormrules"
+        class="login_form"
+      >
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            show-password
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 数据绑定对象
+      loginForm: {
+        username: '',
+        password: '',
+      },
+      // 表单验证规则
+      loginFormrules: {
+        // 用户名是否合法
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' },
+        ],
+        // 密码是否合法
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 15,
+            message: '长度在 6 到 15 个字符',
+            trigger: 'blur',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    resetLoginForm() {
+      // loginForm的实例对象
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      // 使用elform中的validate()方法检查是否通过验证
+      this.$refs.loginFormRef.validate(async (valide) => {
+        if (!valide) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败！')
+        this.$message.success('登录成功！')
+        // 登录成功后的token存储以及页面跳转
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.login_contain {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    background-color: #fff;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
