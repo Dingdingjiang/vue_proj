6db8f44fe40dd333bd973cfa735bf4d40071da2c
Dingdingjiang <45352527+Dingdingjiang@users.noreply.github.com>
2021-07-03 10:00:26 +0800

    add right

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -5,12 +5,44 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>权限列表</el-breadcrumb-item>
     </el-breadcrumb>
-    <el-card> </el-card>
+    <el-card>
+      <el-table :data="rightsTableData" border style="width: 100%">
+        <el-table-column label="#" type="index"> </el-table-column>
+        <el-table-column prop="authName" label="权限名称" width="180">
+        </el-table-column>
+        <el-table-column prop="path" label="路径" width="180">
+        </el-table-column>
+        <el-table-column prop="level" label="权限等级" width="180">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level === '0'">一级</el-tag>
+            <el-tag v-else-if="scope.row.level === '1'" type="success"
+              >二级</el-tag
+            >
+            <el-tag type="warning" v-else>三级</el-tag>
+          </template>
+        </el-table-column></el-table
+      >
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  created() {
+    this.getRightsList()
+  },
+  data() {
+    return {
+      rightsTableData: [],
+    }
+  },
+  methods: {
+    async getRightsList() {
+      const { data: res } = await this.$http.get('rights/list')
+      this.rightsTableData = res.data
+    },
+  },
+}
 </script>
 
 <style lang="less" scoped>
